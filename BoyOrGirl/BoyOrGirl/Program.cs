0f4de88c1005e690e50e1eb0e75953c3b0b4aaf1
Somaiyajannat <somaiyajannat044@gmail.com>
2023-02-12 19:35:53 +0600

    add boy or girl

--- BoyOrGirl/BoyOrGirl/Program.cs
+++ BoyOrGirl/BoyOrGirl/Program.cs
@@ -6,20 +6,36 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        char[] array = new char[26];
-        string input = "wjmzbmr";
-        int count = 0;
+        int[] array = new int[130];
+        string input = Console.ReadLine();
+
+       
         for (int i = 0; i < input.Length; i++)
         {
-            char c = input[i];
-            array[input[i]] = c;
-
-
+            int value = input[i];
+            if(array[value] == 0)
+            {
+                array[value]++;
+            }
         }
+        int count = 0;
         for (int i = 0; i < array.Length; i++)
         {
-            Console.WriteLine(array[i]);
+            if(array[i] != 0)
+            {
+                // Console.WriteLine( i + " "+array[i]);
+                count++;
+            }
+            
         }
+        if ((count %2) == 0)
+        {
+            Console.WriteLine("CHAT WITH HER!");
+        } else
+        {
+            Console.WriteLine("IGNORE HIM!");
+        }
+      
 
 
     }
