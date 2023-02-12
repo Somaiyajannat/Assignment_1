50fe6a052d217b52b67591464aa85f8ec889f901
Somaiyajannat <somaiyajannat044@gmail.com>
2023-02-12 20:08:32 +0600

    add Elephant

---
+++ Elephant/Elephant/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        //while (num > 0)
+        //{
+        //    num = num - 5;
+        //    count++;
+        //}
+        count = num / 5;
+        if (count * 5 < num) count++;
+
+        Console.WriteLine(count);
+    }
+}
